b2bea9e840b9a02c2878251da56b450789640ea7
Josef Diago <josef.diago@gmail.com>
2015-10-28 22:02:35 -0400

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for(:users,
+             :path => '',
+             :path_names => {
+               :sign_in => 'sign-in',
+               :sign_out => 'sign-out',
+               :sign_up => 'sign-up'
+             })
+
   root 'pages#index'
 end
